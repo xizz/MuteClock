7ab56a09bc81aff73827fa8b3c04701720eb478b
GC-Xi <xi@gc.com>
2018-09-11 18:52:47 -0400

    Added test for TimerService

--- app/src/main/java/com/xi_zz/muteclock/TimeUtil.kt
+++ app/src/main/java/com/xi_zz/muteclock/TimeUtil.kt
@@ -9,3 +9,9 @@ val LocalTime.calendar: Calendar
         set(Calendar.HOUR_OF_DAY, hour)
         set(Calendar.MINUTE, minute)
     }
+
+val Long.calendar: Calendar
+    get() = Calendar.getInstance().apply { timeInMillis = this@calendar }
+
+val Calendar.localTime: LocalTime
+    get() = LocalTime.of(get(Calendar.HOUR_OF_DAY), get(Calendar.MINUTE))
---
+++ app/src/test/java/com/xi_zz/muteclock/TimeServiceTest.kt
@@ -0,0 +1,53 @@
+package com.xi_zz.muteclock
+
+import android.app.AlarmManager
+import android.content.Context
+import android.os.Build
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowAlarmManager
+import java.time.LocalTime
+
+@Config(sdk = [Build.VERSION_CODES.O])
+@RunWith(RobolectricTestRunner::class)
+class TimeServiceTest {
+
+    private val application = RuntimeEnvironment.application
+    private lateinit var timerService: TimeService
+    private lateinit var shadowAlarmManager: ShadowAlarmManager
+
+    @Before
+    fun setup() {
+        timerService = TimeServiceImp(application)
+        shadowAlarmManager = shadowOf(application.getSystemService(Context.ALARM_SERVICE) as AlarmManager)
+
+    }
+
+    @Test
+    fun testSetStartTime() {
+        val startTime = LocalTime.of(5, 30)
+        timerService.setStartTime(startTime)
+
+        Assert.assertEquals(startTime, shadowAlarmManager.nextScheduledAlarm.triggerAtTime.calendar.localTime)
+        timerService.observeStartTime().test()
+                .assertNoErrors()
+                .assertValue(startTime)
+    }
+
+    @Test
+    fun testSetEndTime() {
+        val endTime = LocalTime.of(15, 45)
+        timerService.setEndTime(endTime)
+
+        Assert.assertEquals(endTime, shadowAlarmManager.nextScheduledAlarm.triggerAtTime.calendar.localTime)
+        timerService.observeEndTime().test()
+                .assertNoErrors()
+                .assertValue(endTime)
+    }
+}
