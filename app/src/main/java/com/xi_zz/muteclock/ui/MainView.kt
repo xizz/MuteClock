41a7a665d349f5920ca96a47649b0da7cf048400
GC-Xi <xi@gc.com>
2018-10-13 12:27:27 -0400

    Using time picker

--- app/src/main/java/com/xi_zz/muteclock/ui/MainView.kt
+++ app/src/main/java/com/xi_zz/muteclock/ui/MainView.kt
@@ -1,8 +1,10 @@
 package com.xi_zz.muteclock.ui
 
+import android.app.TimePickerDialog
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import android.widget.TimePicker
 import com.xi_zz.muteclock.R
 import com.xi_zz.muteclock.Util.subscribeFromUI
 import com.xi_zz.muteclock.template.BaseModelView
@@ -15,7 +17,6 @@ import kotlinx.android.synthetic.main.main.view.setEndTimeButton
 import kotlinx.android.synthetic.main.main.view.setStartTimeButton
 import kotlinx.android.synthetic.main.main.view.startTimeText
 import java.time.LocalTime
-import java.time.temporal.ChronoUnit
 
 class MainView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -28,14 +29,36 @@ class MainView @JvmOverloads constructor(
 
     lateinit var viewModel: MainViewModel
 
+    private val startTimePickerDialog by lazy {
+        val onTimeSetListener: (view: TimePicker, hour: Int, minute: Int) -> Unit = { view, hour, minute ->
+            viewModel.startTime = LocalTime.of(hour, minute)
+        }
+        TimePickerDialog(context, onTimeSetListener, 0, 0, false)
+    }
+
+    private val endTimePickerDialog by lazy {
+        val onTimeSetListener: (view: TimePicker, hour: Int, minute: Int) -> Unit = { view, hour, minute ->
+            viewModel.endTime = LocalTime.of(hour, minute)
+        }
+        TimePickerDialog(context, onTimeSetListener, 0, 0, false)
+    }
+
     fun setupView() {
         viewModel.state.subscribeFromUI {
             startTimeText.text = it.startTime?.toString() ?: context.getString(R.string.no_start_time)
             endTimeText.text = it.endTime?.toString() ?: context.getString(R.string.no_end_time)
         }.addTo(compositeDisposable)
 
-        setStartTimeButton.setOnClickListener { viewModel.startTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES) }
-        setEndTimeButton.setOnClickListener { viewModel.endTime = LocalTime.now().truncatedTo(ChronoUnit.MINUTES) }
+        setStartTimeButton.setOnClickListener {
+            val time = LocalTime.now()
+            startTimePickerDialog.updateTime(time.hour, time.minute)
+            startTimePickerDialog.show()
+        }
+        setEndTimeButton.setOnClickListener {
+            val time = LocalTime.now()
+            endTimePickerDialog.updateTime(time.hour, time.minute)
+            endTimePickerDialog.show()
+        }
         cancelStartTime.setOnClickListener { viewModel.startTime = null }
         cancelEndTime.setOnClickListener { viewModel.endTime = null }
     }
